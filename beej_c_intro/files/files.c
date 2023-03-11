5872b87c9a507e7945863f50262c3bceac724515
Paulo Black <pblack@goodwaygroup.com>
2023-03-11 06:41:54 -0500

    init ostep

--- beej_c_intro/files/files.c
+++ beej_c_intro/files/files.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
+    // File print to stdout
+    fprintf(stdout, "printing to stdout\n");
+    fprintf(stderr, "printing to stderr\n");
 
+    FILE* fp;
+
+    fp = fopen("hello.txt", "r");
+
+    char c = fgetc(fp);
+
+    printf("First char of file: %c\n", c);
+    fclose(fp);
 }
